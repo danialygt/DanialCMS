3c97f5e7f7d88f97bd805abb69713476fba49013
Danialygt <Danialygt@DESKTOP-5B55USK>
2020-11-05 21:07:18 +0330

    move files on right place

--- src/EndPoints/DanialCMS.EndPoints.WebUI/Infrastructures/Identity/CustomPasswordValidator.cs
+++ src/EndPoints/DanialCMS.EndPoints.WebUI/Infrastructures/Identity/CustomPasswordValidator.cs
@@ -1,18 +1,17 @@
-﻿using DanialCMS.EndPoints.WebUI.Infrastructures.Identity;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DanialCMS.EndPoints.WebUI.Infrastructures
+namespace DanialCMS.EndPoints.WebUI.Infrastructures.Identity
 {
     public class CustomPasswordValidator : PasswordValidator<User>
     {
-    public override async Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user, string password)
-    {
-        
-        var baseResult = await base.ValidateAsync(manager, user, password);
+        public override async Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user, string password)
+        {
+
+            var baseResult = await base.ValidateAsync(manager, user, password);
             var erros = baseResult.Succeeded ? new List<IdentityError>()
                 : baseResult.Errors.ToList();
             if (password.ToLower().Contains(user.UserName.ToLower()))
@@ -27,7 +26,7 @@ namespace DanialCMS.EndPoints.WebUI.Infrastructures
             // inja vadilate ahto minevisi!
 
 
-            return await Task.FromResult((erros.Count == 0) ? IdentityResult.Success :
+            return await Task.FromResult(erros.Count == 0 ? IdentityResult.Success :
                 IdentityResult.Failed(erros.ToArray()));
         }
     }
